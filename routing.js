077978f414e312c41594aed337b510b066907b90
AbdelhakBLD <abdelhak.belaid@outlook.com>
2021-01-14 12:00:15 +0100

    update routing.js

---
+++ routing.js
@@ -0,0 +1,10 @@
+import { userController } from "./controllers/UserController";
+
+//création manuelle d'un routeur
+//nous plaçons toutes nos routes dans une fonction
+export const setRouting = (server) => {
+    server.get('/users', userController.findAll);
+    server.post('/users', userController.save)
+    server.get('/users/:id', userController.findById)
+    server.delete('/users/:id', userController.deleteById)
+}
