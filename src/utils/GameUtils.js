1a2d4f2fe04e9eadbda06c9bf1dba4826bbbb5ee
Julien Grenier <grenier.julien5@gmail.com>
2021-01-15 15:44:18 +0100

    update new game

--- src/utils/GameUtils.js
+++ src/utils/GameUtils.js
@@ -4,7 +4,7 @@ class GameUtils {
         const p1Name = this.fieldEmpty(game.player1.name) ? "Player 1" : game.player1.name;
         const p2Name = this.fieldEmpty(game.player2.name) ? "Player 2" : game.player2.name;
         const p1Jeu = this.fieldEmpty(game.player1.jeu) ? 0 : game.player1.jeu;
-        const p2jeu = this.fieldEmpty(game.player2.jeu) ? 0 : game.player2.jeu;
+        const p2Jeu = this.fieldEmpty(game.player2.jeu) ? 0 : game.player2.jeu;
         const p1Set = this.fieldEmpty(game.player1.set) ? 0 : game.player1.set;
         const p2Set = this.fieldEmpty(game.player2.set) ? 0 : game.player2.set;
 
@@ -12,7 +12,7 @@ class GameUtils {
             "player1Name": p1Name,
             "player2Name": p2Name,
             "player1Jeu": p1Jeu,
-            "player2Jeu": p2jeu,
+            "player2Jeu": p2Jeu,
             "player1Set": p1Set,
             "player2Set": p2Set
         }
