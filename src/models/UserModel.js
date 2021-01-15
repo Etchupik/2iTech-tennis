52e60ed0cd861cf275bdafe25ba10914b83611ac
Christian <c.jr.camelot@gmail.com>
2021-01-15 09:20:48 +0100

    préparation du side client

--- src/models/UserModel.js
+++ src/models/UserModel.js
@@ -1,7 +1,8 @@
 import mongoose from 'mongoose';
 
 const gameSchema = new mongoose.Schema({
-	userName: String
+	userName: String,
+	userPoints: String
 });
 
 export const userModel = mongoose.model('Game', gameSchema);
