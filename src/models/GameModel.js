6cd081cd4d81cc7f5226e12d2d236d9e4bce367a
Christian <c.jr.camelot@gmail.com>
2021-01-15 13:08:11 +0100

    changement entre player et game model

--- src/models/GameModel.js
+++ src/models/GameModel.js
@@ -3,8 +3,11 @@ import mongoose from 'mongoose';
 const gameSchema = new mongoose.Schema({
     player1Name: String,
     player2Name: String,
-    player1Points: Number,
-    player2Points: Number
+    player1Jeu: Number,
+    player2Jeu: Number,
+    player1Set: Number,
+	player2Set: Number,
+	playerWinner: Boolean 
 });
 
 export const gameModel = mongoose.model('Game', gameSchema);
--- src/models/PlayerModel.js
+++ src/models/PlayerModel.js
@@ -3,9 +3,6 @@ import mongoose from 'mongoose';
 const playerSchema = new mongoose.Schema({
 	playerName: String,
 	playerPoints: Number,
-	playerJeu: Number,
-	playerSet: Number,
-	playerWinner: Boolean 
 });
 
 export const playerModel = mongoose.model('Player', playerSchema);
